494a9fcb52fcb3447478c0adb60e8e47315f4bd2
michyo <michyox@gmail.com>
2023-04-13 13:25:09 +0900

    Initial commit

---
+++ praat.lua
@@ -0,0 +1,274 @@
+--**********************************************************************
+--* Praat - Dialogue system for LOVE                                   *
+--* Copyright (C) 2023 michyo (Michiyo Tagami)                         *
+--* Licensed under the MOUSEY license                                  *
+--* See LICENSE.txt for more information about the license.            *
+--**********************************************************************
+
+utf8 = require("utf8")
+
+local praat = {}
+
+praat.table, praat.lines = {}, {}
+praat.at, praat.interval, praat.nowLine = 0, 0.1, 1
+praat.nameBGarea, praat.textBGarea = nil, nil
+praat.textBGcolor, praat.textLINEcolor, praat.textLINEwidth = {0, 0, 0, 0.8}, {1, 1, 1}, 2
+praat.textFGarea = nil; praat.textFGcolor = {1, 1, 1}; praat.textFGfont = nil
+praat.textFGlineh = 0; praat.textMaxWidth = 0; praat.textMaxLines = 0
+praat.nameBGcolor, praat.nameLINEcolor, praat.nameLINEwidth = {0, 0, 0, 0.8}, {1, 1, 1}, 2
+praat.nameFGlt = nil; praat.nameFGcolor = {1, 1, 1}; praat.nameFGfont = nil
+praat.name = "";
+praat.state = 0 -- 0:Waiting inputs 1:Showing 2:Waiting reset
+praat.waitCursor = nil
+praat.wcAt, praat.wcInterval, praat.wcWidth, praat.wcAnidir, praat.wcXY = 0, 0.05, 16, true, {784, 584}
+
+praat.init = function(praat)
+  praat.textFGfont = love.graphics.getFont()
+  praat.nameFGfont = love.graphics.getFont()
+  repeat
+  until (praat.textFGfont and praat.nameFGfont)
+end
+
+praat.setWaitCursor = function(praat, waitCursor, wcXY)
+  praat.waitCursor = waitCursor
+  praat.wcXY = wcXY
+end
+
+praat.setInterval = function(praat, interval)
+  praat.interval = interval
+end
+
+praat.setTextBG = function(praat, textBGarea, textBGcolor, textLINEcolor, textLINEwidth)
+  if textBGarea then
+    praat.textBGarea = textBGarea
+  end
+  if textBGcolor then
+    praat.textBGcolor = textBGcolor
+  end
+  if textLINEcolor then
+    praat.textLINEcolor = textLINEcolor
+  end
+  if textLINEwidth then
+    praat.textLINEwidth = textLINEwidth
+  end
+end
+
+praat.setTextFG = function(praat, textFGarea, textFGcolor, textFGfont, textFGlineh)
+  if textFGarea then
+    praat.textFGarea = textFGarea
+    praat.textMaxWidth = textFGarea[3]
+    if textFGlineh then
+      praat.textFGlineh = textFGlineh
+      praat.textMaxLines = math.floor(textFGarea[4]/textFGlineh)
+      --print(praat.textMaxLines)
+    end
+  end
+  if textFGcolor then
+    praat.textFGcolor = textFGcolor
+  end
+  if textFGfont then
+    praat.textFGfont = textFGfont
+  end
+end
+
+praat.setNameBG = function(praat, nameBGarea, nameBGcolor, nameLINEcolor, nameLINEwidth)
+  if nameBGarea then
+    praat.nameBGarea = nameBGarea
+  end
+  if nameBGcolor then
+    praat.nameBGcolor = nameBGcolor
+  end
+  if nameLINEcolor then
+    praat.nameLINEcolor = nameLINEcolor
+  end
+  if nameLINEwidth then
+    praat.nameLINEwidth = nameLINEwidth
+  end
+end
+
+praat.setNameFG = function(praat, nameFGlt, nameFGcolor, nameFGfont)
+  if nameFGlt then
+    praat.nameFGlt = nameFGlt
+  end
+  if nameFGcolor then
+    praat.nameFGcolor = nameFGcolor
+  end
+  if nameFGfont then
+    praat.nameFGfont = nameFGfont
+  end
+end
+
+praat.addline = function(praat, name, line, reset)
+  local newline = {}
+  newline.reset = reset
+  newline.chars = {}
+  newline.name = name
+  for pos, code in utf8.codes(line) do
+    table.insert(newline.chars, utf8.char(code))
+    if (utf8.char(code) == "\n") then
+      --print("改行！")
+    end
+  end
+  --for i=0,#(newline.chars) do
+  --  print(newline.chars[i])
+  --end
+  table.insert(praat.table,newline)
+end
+
+praat.goNextDialogue = function(praat)
+  if (praat.table[1]["reset"] and (praat.state == 2)) then
+    praat.name = ""
+    praat.lines = {}
+    praat.nowLine = 1
+    praat.table[1] = nil
+    praat:newLine(praat)
+  end
+end
+
+praat.setState = function(praat, state)
+  if (state ~= praat.state) then
+    praat.state = state
+    praat.wcAt = 0
+    praat.wcAnidir = true; praat.wcWidth = 16
+  end
+end
+
+praat.newLine = function(praat)
+  --print("tableno:"..#(praat.table))
+  if (#(praat.table)>1) then
+    for tmpI=#(praat.table),2 do
+      praat.table[tmpI-1] = praat.table[tmpI]
+    end
+    table.remove(praat.table, #(praat.table))
+    praat.nowLine = praat.nowLine + 1
+    if (praat.nowLine > praat.textMaxLines) then
+      for tmpI = praat.textMaxLines, 2, -1 do
+        praat.lines[tmpI-1] = praat.lines[tmpI]
+      end
+      praat.lines[praat.textMaxLines] = ""
+      praat.nowLine = praat.textMaxLines
+    end
+    if ((praat.name ~= praat.table[1]["name"]) and (praat.table[1]["name"] ~= nil)) then
+      praat.lines = {}
+      praat.nowLine = 1
+    end
+  else
+    praat:setState(0)
+  end
+end
+
+praat.update = function(praat, dt)
+  praat.at = praat.at + dt
+  if (praat.at >= praat.interval) then
+    praat.at = praat.at - praat.interval
+    if praat.table[1] then
+      if praat.table[1]["chars"][1] then
+        praat.state = 1
+        if praat.table[1]["name"] then
+          praat.name = praat.table[1]["name"]
+        end
+        if not praat.lines[praat.nowLine] then
+          praat.lines[praat.nowLine] = ""
+        end
+        if (praat.table[1]["chars"][1] == "\n") then
+          praat.nowLine = praat.nowLine + 1
+          if (praat.nowLine > praat.textMaxLines) then
+            for tmpI = praat.textMaxLines, 2, -1 do
+              praat.lines[tmpI-1] = praat.lines[tmpI]
+            end
+            praat.lines[praat.textMaxLines] = ""
+            praat.nowLine = praat.textMaxLines
+          end
+          if not praat.lines[praat.nowLine] then
+            praat.lines[praat.nowLine] = ""
+          end
+        else
+          if (praat.textFGfont:getWidth(praat.lines[praat.nowLine] .. praat.table[1]["chars"][1]) > praat.textMaxWidth) then
+            --print(praat.textMaxWidth)
+            praat.nowLine = praat.nowLine + 1
+            if (praat.nowLine > praat.textMaxLines) then
+              for tmpI = praat.textMaxLines, 2, -1 do
+                praat.lines[tmpI-1] = praat.lines[tmpI]
+              end
+              praat.lines[praat.textMaxLines] = ""
+              praat.nowLine = praat.textMaxLines
+            end
+            if not praat.lines[praat.nowLine] then
+              praat.lines[praat.nowLine] = ""
+            end
+          end
+          praat.lines[praat.nowLine] = praat.lines[praat.nowLine] .. praat.table[1]["chars"][1]
+        end
+        table.remove(praat.table[1]["chars"], 1)
+        --print(praat.lines[praat.nowLine])
+      else
+        --print("newline")
+        if not praat.table[1]["reset"] then
+          praat:newLine()
+        else
+          praat:setState(2);
+        end
+      end
+    end
+  end
+  --love.graphics.setFont(tmpFont)
+  if (praat.state == 2) then
+    praat.wcAt = praat.wcAt + dt
+    if (praat.wcAt > praat.wcInterval) then
+      praat.wcAt = praat.wcAt - praat.wcInterval
+      if praat.wcAnidir then
+        praat.wcWidth = praat.wcWidth - 1
+        if (praat.wcWidth < 0) then
+          praat.wcWidth = 0
+          praat.wcAnidir = false
+        end
+      else
+        praat.wcWidth = praat.wcWidth + 1
+        if (praat.wcWidth > 16) then
+          praat.wcWidth = 16
+          praat.wcAnidir = true
+        end
+      end
+    end
+  end
+end
+
+praat.draw = function(praat)
+  local tmpColor = {love.graphics.getColor()}
+  local tmpLineWidth = love.graphics.getLineWidth()
+  local tmpFont = love.graphics.getFont()
+  if praat.nameBGarea then
+    love.graphics.setColor(praat.nameBGcolor)
+    love.graphics.polygon("fill", praat.nameBGarea)
+    love.graphics.setColor(praat.nameLINEcolor)
+    love.graphics.setLineWidth(praat.nameLINEwidth)
+    love.graphics.polygon("line", praat.nameBGarea)
+  end
+  if praat.textBGarea then
+    love.graphics.setColor(praat.textBGcolor)
+    love.graphics.polygon("fill", praat.textBGarea)
+    love.graphics.setColor(praat.textLINEcolor)
+    love.graphics.setLineWidth(praat.textLINEwidth)
+    love.graphics.polygon("line", praat.textBGarea)
+  end
+  love.graphics.setFont(praat.nameFGfont)
+  love.graphics.setColor(praat.nameFGcolor)
+  love.graphics.print(praat.name, praat.nameFGlt[1], praat.nameFGlt[2])
+  love.graphics.setFont(praat.textFGfont)
+  love.graphics.setColor(praat.textFGcolor)
+  for tmpI = 1,praat.textMaxLines do
+    if praat.lines[tmpI] then
+      love.graphics.print(praat.lines[tmpI], praat.textFGarea[1], praat.textFGarea[2] + praat.textFGlineh * (tmpI-1))
+    end
+  end
+  if (praat.state == 2) then
+    --print("cursor:"..praat.wcWidth)
+    love.graphics.draw(praat.waitCursor, praat.wcXY[1] + (16 - praat.wcWidth)/2, praat.wcXY[2], 0, praat.wcWidth/16, 1)
+  end
+  --戻し
+  love.graphics.setColor(tmpColor)
+  love.graphics.setLineWidth(tmpLineWidth)
+  love.graphics.setFont(tmpFont)
+end
+
+return praat
---
+++ readme.md
@@ -0,0 +1,23 @@
+Praat - Dialogue system for LÖVE
+================================
+
+Copyright (C) 2023 michyo (Michiyo Tagami)  
+
+Overview
+--------
+
+Praat is a dialogue system library for LÖVE.  
+With Praat, you can easily create dialogue scenes.  
+
+Version History
+---------------
+
+#### Version 0.01 (2023/04/13) - Current version  
+
+* Initial release
+
+License
+-------
+
+Licensed under the MOUSEY license  
+See LICENSE.txt for more information about the license.  
